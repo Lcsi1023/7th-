d5263e63caa843ef52b27794550da82190fbc895
Lcsi1023 <129623112+Lcsi1023@users.noreply.github.com>
2023-10-11 02:20:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,2 @@
-import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-
-import pandas as pd
-if uploaded_file is not None:
-    df = pd.read_csv(
-        uploaded_file,
-        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-        engine='python',
-        na_values='-',
-        header=None)
-
-    st.markdown('### アクセスログ（先頭5件）')
-    st.write(df.head(5))
-
+st.set_page_config(page_title="メインページ", page_icon='icon.png')
+st.title("Multiple OSS Access Log Analyzer")
