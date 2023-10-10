6add8e80b3f10b0eb3950271dbc18ef35a321150
Lcsi1023 <129623112+Lcsi1023@users.noreply.github.com>
2023-10-11 02:29:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,21 @@
 import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+from googleapiclient.discovery import build
+
+# YouTube Data API v3のAPIキー
+api_key = "AIzaSyCckGeP71CF5741HVdr5sO7-2fEVqORgc0"
+
+# YouTube APIクライアントの作成
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# ビデオID
+video_id = "x_BjvhMW9TE"
+
+# ビデオの統計情報を取得
+request = youtube.videos().list(part="statistics", id=video_id)
+response = request.execute()
+
+# 再生回数を取得
+view_count = response['items'][0]['statistics']['viewCount']
+
+# Streamlitで表示
+st.line_chart(view_count)
